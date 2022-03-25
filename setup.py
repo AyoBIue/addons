d05496843bf8eeb8ef02a9948e61a33fd08a6595
AyoBIue <57001134+AyoBIue@users.noreply.github.com>
2022-03-25 18:24:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name = 'addons',
-  version = '0.0.2',
+  version = '0.0.3',
   author = 'AyoBlue',
   author_email = 'ajtorres0119@gmail.com',
   description = ('Just some basic addons..'),
