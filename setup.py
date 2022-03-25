d8e2ce4773618c1ce5ee39859e2b8e8f49b2b9af
AyoBIue <57001134+AyoBIue@users.noreply.github.com>
2022-03-25 18:20:18 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name = 'addons',
+  version = '0.0.1',
+  author = 'AyoBlue',
+  author_email = 'ajtorres0119@gmail.com',
+  description = ('Just some basic addons..')
+)
